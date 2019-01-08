a9dc77a306fa8824c6a0ef02e6bf7cc7fc2f2d91
Darcy Cowan <dwc1@students.waikato.ac.nz>
2019-01-08 15:23:42 +1300

    nope, not a clue. give up

--- src/ictgradschool/industry/swingworker/ex05/ImageShape.java
+++ src/ictgradschool/industry/swingworker/ex05/ImageShape.java
@@ -10,6 +10,8 @@ import java.net.URI;
 import java.net.URL;
 import java.util.List;
 
+import static javax.imageio.ImageIO.*;
+
 /**
  * A shape which is capable of loading and rendering images from files / Uris / URLs / etc.
  */
@@ -28,7 +30,9 @@ public class ImageShape extends Shape {
     public ImageShape(int x, int y, int deltaX, int deltaY, int width, int height, URL url) {
         super(x, y, deltaX, deltaY, width, height);
 
-        ImageGetter imageGetter = new ImageGetter(url,width,height,image);
+
+
+        ImageGetter imageGetter = new ImageGetter(url,width,height);
         imageGetter.execute();
 
 //        try {
@@ -44,37 +48,41 @@ public class ImageShape extends Shape {
 
     }
 
+    //My Swinger
     private class ImageGetter extends SwingWorker<Void,Image>{
         URL url;
         int width;
         int height;
         Image image;
 
-        public ImageGetter(URL url,int width,int height,Image image){
+        public ImageGetter(URL url,int width,int height){
             this.url=url;
             this.width=width;
-            this.image=image;
+            this.height=height;
+//            this.image=image;
         }
 
         @Override
         protected Void doInBackground() throws Exception {
             try {
-                Image image = ImageIO.read(url);
-                if (width == image.getWidth(null) && height == image.getHeight(null)) {
-                    this.image = image;
-                } else {
-                    this.image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-                }
+                Image image = read(url);
+                publish(image);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            publish(image);
             return null;
         }
 
         @Override
         protected void process(List<Image> chunks) {
-            super.process(chunks);
+            for (Image image:chunks
+                 ) {
+                    if (width == image.getWidth(null) && height == image.getHeight(null)) {
+                        this.image = image;
+                    } else {
+                        this.image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+                    }
+            }
         }
     }
 
