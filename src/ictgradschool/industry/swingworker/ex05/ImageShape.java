f74ef89eaf3bbccc74993a3aa6a807bbcdea481b
Darcy Cowan <dwc1@students.waikato.ac.nz>
2019-01-08 14:28:47 +1300

    doing ex05. not sure about this

--- src/ictgradschool/industry/swingworker/ex05/ImageShape.java
+++ src/ictgradschool/industry/swingworker/ex05/ImageShape.java
@@ -1,12 +1,14 @@
 package ictgradschool.industry.swingworker.ex05;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.util.List;
 
 /**
  * A shape which is capable of loading and rendering images from files / Uris / URLs / etc.
@@ -26,17 +28,54 @@ public class ImageShape extends Shape {
     public ImageShape(int x, int y, int deltaX, int deltaY, int width, int height, URL url) {
         super(x, y, deltaX, deltaY, width, height);
 
-        try {
-            Image image = ImageIO.read(url);
-            if (width == image.getWidth(null) && height == image.getHeight(null)) {
-                this.image = image;
-            } else {
-                this.image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        ImageGetter imageGetter = new ImageGetter(url,width,height,image);
+        imageGetter.execute();
+
+//        try {
+//            Image image = ImageIO.read(url);
+//            if (width == image.getWidth(null) && height == image.getHeight(null)) {
+//                this.image = image;
+//            } else {
+//                this.image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    private class ImageGetter extends SwingWorker<Void,Image>{
+        URL url;
+        int width;
+        int height;
+        Image image;
+
+        public ImageGetter(URL url,int width,int height,Image image){
+            this.url=url;
+            this.width=width;
+            this.image=image;
         }
 
+        @Override
+        protected Void doInBackground() throws Exception {
+            try {
+                Image image = ImageIO.read(url);
+                if (width == image.getWidth(null) && height == image.getHeight(null)) {
+                    this.image = image;
+                } else {
+                    this.image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            publish(image);
+            return null;
+        }
+
+        @Override
+        protected void process(List<Image> chunks) {
+            super.process(chunks);
+        }
     }
 
 
